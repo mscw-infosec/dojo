01d4259a1c33dcfd91df0ac6f48c1aff32f1737d
JustMarfix <rezak3031@ya.ru>
2025-11-05 17:35:51 +0300

    add localization & our stuff

--- dojo_plugin/pages/belts.py
+++ dojo_plugin/pages/belts.py
@@ -6,9 +6,9 @@ from ..utils.awards import get_belts
 belts = Blueprint("pwncollege_belts", __name__)
 
 
-@belts.route("/belts")
-def view_belts():
-    return render_template("belts.html", belt_data=get_belts())
+# @belts.route("/belts")
+# def view_belts():
+#     return render_template("belts.html", belt_data=get_belts())
 
 
 @belts.route("/belt/<color>.svg")
